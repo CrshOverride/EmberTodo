7421f6d04e377f45971893702fa82749de53f7f2
Justin Niessner <CrshOverride@gmail.com>
2015-02-16 11:36:32 -0500

    More debugging

--- deploy.sh
+++ deploy.sh
@@ -101,17 +101,17 @@ selectNodeVersion () {
 NODE_EXE="D:\\Program Files (x86)\\nodejs\\0.10.32\\node.exe"
 NPM_CMD="\"D:\\Program Files (x86)\\nodejs\\0.10.32\\node.exe\" \"D:\\Program Files (x86)\\npm\\1.4.28\\node_modules\\npm\\bin\\npm-cli.js\""
 
-echo $NODE_EXE
-echo $NPM_CMD
+echo Using node from: $NODE_EXE
+echo Using npm command: $NPM_CMD
 
 echo Handling node.js deployment.
 
 # 3. Install everything
 #npm config set strict-ssl false
 
-echo Installing Ember CLI
-$NPM_CMD install -g ember-cli
-exitWithMessageOnError "ember-cli failed"
+# echo Installing Ember CLI
+# $NPM_CMD install -g ember-cli
+# exitWithMessageOnError "ember-cli failed"
 
 # echo Installing Bower
 # npm install -g bower
