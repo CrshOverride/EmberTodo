0ebe463976fa5b6a632f8f25f7db42cc1c1a2f6d
Justin Niessner <CrshOverride@gmail.com>
2015-02-16 16:50:25 -0500

    Add npm install back to the batch file

--- deploy.cmd
+++ deploy.cmd
@@ -103,6 +103,10 @@ echo 1.1 Remove cached version of PhantomJS
 rmdir "D:\local\Temp\phantomjs"
 IF !ERRORLEVEL! NEQ 0 goto error
 
+echo 1.2 Execute npm install
+call :ExecuteCmd !NPM_CMD! install --production
+IF !ERRORLEVEL! NEQ 0 goto error
+
 :: 1. KuduSync
 ::IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
 ::  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
