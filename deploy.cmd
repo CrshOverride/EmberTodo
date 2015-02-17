b347eacbfe376367ca5bd018dcb4bc305b57b844
Justin Niessner <CrshOverride@gmail.com>
2015-02-16 22:03:21 -0500

    Try adding a web.config and see if it works

--- deploy.cmd
+++ deploy.cmd
@@ -98,6 +98,7 @@ IF !ERRORLEVEL! NEQ 0 goto error
 echo 3. Publish compiled assets
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
   call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%\dist" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
+  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%\web.config" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%"
   IF !ERRORLEVEL! NEQ 0 goto error
 )
 
